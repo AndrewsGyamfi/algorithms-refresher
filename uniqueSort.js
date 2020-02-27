488d911920da21443a3717203ba741eae187a20a
Andrews Gyamfi <andrews.gyamfi@crownbet.com.au>
2020-02-27 22:54:57 +1100

    chore: [master] - Remove

--- uniqueSort.js
+++ uniqueSort.js
@@ -1,5 +1,14 @@
 const ascending = (a, b) => a - b;
 
+/**
+ * @name uniqueSortOne
+ * @param {array} list - items to sort
+ *
+ * Uses an object (`seen` in this case) to
+ * track items already added to the array.
+ *
+ * @returns {array} sorted array
+ */
 const uniqueSortOne = list => {
   const seen = {};
   const sorted = [];
@@ -7,18 +16,35 @@ const uniqueSortOne = list => {
   for (let i = 0; i < list.length; i++) {
     const thisItem = list[i];
 
-    if (seen[thisItem]) {
-      continue;
+    if (!seen[thisItem]) {
+      sorted.push(thisItem);
+      seen[thisItem] = true;
     }
-    sorted.push(thisItem);
-    seen[thisItem] = true;
   }
 
   return sorted.sort(ascending);
 };
 
+/**
+ * @name uniqueSortTwo
+ * @param {array} list - items to sort
+ * Set data structure maintains only
+ * unique values and also implements an
+ * iterable interface. So you can spread,
+ * and spread and spread :)
+ *
+ * @returns {array} sorted array
+ */
 const uniqueSortTwo = list => [...new Set(list)].sort(ascending);
 
+/**
+ * @name uniqueSortThree
+ * @param {array} list - items to sort
+ * Unnecessary complications but I just
+ * wanted to use reduce :wink
+ *
+ * @returns {array} sorted array
+ */
 const uniqueSortThree = list =>
   Object.keys(
     list.reduce(
