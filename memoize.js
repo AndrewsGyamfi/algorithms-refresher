374225ff4b1a1e3e6b83d209782d0e164ed1a08c
Andrews Gyamfi <andrews.gyamfi@crownbet.com.au>
2020-02-27 23:31:14 +1100

    chore: [master] - Add generic memoization function

---
+++ memoize.js
@@ -0,0 +1,22 @@
+/**
+ * Basic generic memoization
+ * function
+ * @name memoize
+ * @param {Function} cb - callback function
+ * @returns {Function}
+ */
+const memoize = cb => {
+  const cache = {};
+
+  return (...args) => {
+    const cacheKey = args.join();
+
+    if (cacheKey in cache) {
+      return cache[cacheKey];
+    }
+
+    cache[cacheKey] = cb(...args);
+
+    return cache[cacheKey];
+  };
+};
